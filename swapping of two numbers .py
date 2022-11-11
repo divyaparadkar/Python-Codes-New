ea45836c18213817145b571a13e2ed368a799870
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-12 01:03:47 +0530

    Add files via upload

---
+++ swapping of two numbers .py
@@ -0,0 +1,9 @@
+a=input("Enter the value of a:")
+b=input("Enter the value of b:")
+print("Value before swapping ")
+print("a=",a,"b=",b)
+temp = a
+a=b
+b=temp
+print("Value after swapping :")
+print("a=",a,"b=",b)
