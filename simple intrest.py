721ad47a41b86d3189b3f58663f793547265cb81
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-11 22:13:16 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,10 @@
+#simple intrest  is si
+#formula of si is p*r*t/100.
+print("Principle is :")
+p=int(input())
+print("rate is :")
+r=int(input())
+print("time is :")
+t=int(input())
+si=p*r*t/100
+print("The Simple intrest is :",si)
