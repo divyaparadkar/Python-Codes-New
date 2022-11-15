1a648f2a03e1ca483e924f67d8d4eb6b47abb4a6
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-16 00:08:39 +0530

    Add files via upload

---
+++ List in python.py
@@ -0,0 +1,8 @@
+# print the list in py.
+list = [1, 2, 3, 4, 5, 6]
+print(len(list))#find the length of list
+print(list.index(6))#find the index element that occure first.
+print(list.count(10))#find the count of the elemets .
+print(sorted(list))#print stored list but not change original
+list.sort(reverse=True)#sort original list
+print(list)
