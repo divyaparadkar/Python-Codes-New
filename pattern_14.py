205140ab869294a652629a8003b10a21c7d1ad50
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-24 00:16:44 +0530

    Add files via upload

---
+++ pattern_14.py
@@ -0,0 +1,19 @@
+n = 5
+for i in range(n):
+    for j in range(i + 1):
+        print(" ", end=' ')
+    for j in range(i, n-1):
+        print('%', end=' ')
+    for j in range(i+1):
+        print('%', end=' ')
+    print()
+    for i in range(n):
+        for j in range(i +1):
+            print(' ', end=' ')
+        for j in range(i, n):
+            print('%', end=' ')
+        for i in range(i, n):
+            print('%', end=' ')
+        print()
+
+
