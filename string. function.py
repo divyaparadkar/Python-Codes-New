1d05e5cb41f46e94b6deb28382b8c40e2eaad675
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-30 22:34:42 +0530

    Add files via upload

---
+++ string. function.py
@@ -0,0 +1,22 @@
+mystr = "Divya i know you can do it :"
+print(mystr[0:4])
+print(len(mystr))
+print(mystr[0:5:2])
+print(mystr[::1])
+print(mystr[::2])
+print(mystr[15:19])
+print(mystr[::-2])
+
+#functions
+#True and False are two bollen character in python.
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.count("0"))
+print(mystr.capitalize())
+print(mystr.find("it"))
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.replace("it", "this"))
+
+
+
