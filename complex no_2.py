abb9768ee62cc73645c9e6efea74dde3ed53c6b2
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-12-04 02:05:47 +0530

    Add files via upload

---
+++ complex no_2.py
@@ -0,0 +1,11 @@
+# decimal number system
+n1 =0o17
+n2 = 0B111001100
+n3 =0X1c2
+
+n = int(n1)
+print('octal 17',n)
+n = int(n2)
+print('Binary 11100010', n)
+n =int(n3)
+print('hecadecimal 1c2 = ',n)
