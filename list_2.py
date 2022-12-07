b4da1e8eea9b633044d0d497f79839f5d47db239
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-12-07 21:16:52 +0530

    Add files via upload

---
+++ list_2.py
@@ -0,0 +1,7 @@
+list = [1, 3, 7, 'Divya', 9, 'piyush', 'div', 45]
+print(list)
+print(list[3:4])#print 'Divya'
+print(list[0])#print 1
+print(list[7:-1])#print []
+print(list[-7])#print 3
+print(list *2)
