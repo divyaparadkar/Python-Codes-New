b2b8b86d888746a1c74db2e0809d508075cab492
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-01-06 17:01:47 +0530

    Add files via upload

---
+++ array_1.py
@@ -0,0 +1,4 @@
+#multiplication in python with the help of array.
+import numpy as np 
+arr = np.array([1,2,3,4,5,6])
+print(arr*3)
---
+++ displayelements.py
@@ -0,0 +1,5 @@
+import numpy as ds
+arr =ds.array([1,2,3,4,5,8,9])
+print(arr[0])
+print(arr[1])
+print(arr[2])
