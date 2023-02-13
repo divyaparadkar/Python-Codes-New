2ef77e1afa970cab5c8ebaf64fdf18265cb522c2
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-02-13 17:48:11 +0530

    Add files via upload

---
+++ GCD.PY.py
@@ -0,0 +1,14 @@
+#greatest  comman divisor factor
+
+def gcd(a, b):
+    assert int(a) == a and int(b) == b, 'the number must be interger only'
+    if a < 0:
+        a = -1*a
+    if b < 0:
+        b = -1*b
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+print(gcd(48,18))
