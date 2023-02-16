d81bf354c79dae7173c0660cdda1631f8b69eff0
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-02-16 23:26:54 +0530

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,9 @@
+def hello(name,add):
+    print(name,'and',add)
+hello(add='bhopal',name='divya')# with the help of keyword argument value  are assgine with name and address 
+
+
+def hello(name,add):
+    print(name ,'and',  add)
+hello('divya', 'bhopal')
+
