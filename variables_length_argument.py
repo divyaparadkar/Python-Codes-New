603b8dff98faf8c71cdb758d545008144f44a9f3
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-02-16 23:33:55 +0530

    Add files via upload

---
+++ variables_length_argument.py
@@ -0,0 +1,10 @@
+import sys
+l=[1,2,3,4,5]
+t=(1,2,3,4,5)
+s={1,2,3,4,5}
+sys.getsizeof(l)
+sys.getsizeof(t)
+sys.getsizeof(s)
+print(sys.getsizeof(l))
+print(sys.getsizeof(t))
+print(sys.getsizeof(s))
