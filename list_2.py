ba37e3dd5618114cc4df7f2bc513fedbddcc2e67
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-02-21 23:39:33 +0530

    Add files via upload

--- list_2.py
+++ list_2.py
@@ -1,7 +1,19 @@
-list = [1, 3, 7, 'Divya', 9, 'piyush', 'div', 45]
-print(list)
-print(list[3:4])#print 'Divya'
-print(list[0])#print 1
-print(list[7:-1])#print []
-print(list[-7])#print 3
-print(list *2)
+# Update/ Insert - List
+
+mylist =[1,2,3,4,5,6]
+print(mylist)
+mylist[3]=34 
+mylist[0]=56
+print(mylist)
+
+
+mylist =[1,2,3,4,5,6]
+print(mylist)
+mylist.insert(4,24) #insert method ------------> O(n)
+print(mylist)
+
+mylist.append(55)# append method------------>O(1)
+
+newlist=[12,23,34,67,78]
+mylist.extend(newlist)#extend method ----------->O(n)
+print(mylist)
