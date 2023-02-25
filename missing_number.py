e4cb9d534fcb58a46090569e36266ff107922d59
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-02-26 01:14:13 +0530

    Add files via upload

---
+++ missing_number.py
@@ -0,0 +1,9 @@
+# find the missing nummber in an integer array of 1 to 100
+
+mylist=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20]
+def findMissing(list,n):
+    sum1 =20*21/2
+    sum2 =sum(list)
+    print(sum1-sum2)
+
+findMissing(mylist,100)
