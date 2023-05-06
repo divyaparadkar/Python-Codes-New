21bd947300bd05372fc3a7774a3cb99a661e2b42
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-05-06 23:39:50 +0530

    Add files via upload

--- H_pattern.py
+++ H_pattern.py
@@ -1,3 +1,7 @@
 for i in range(7):
     for j in range(5):
-        if j==0 
+        if j==0 or j==4 or (i==3 and j>0 and j<4) and (j>1 or j<5): 
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
